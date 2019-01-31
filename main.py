94cec81c38959160d243e32b41e8260b74ab0d9d
Tom Archer <thomastarcher@icloud.com>
2019-01-31 21:23:01 +0000

    Add fn which prompts the user for starting lives

--- main.py
+++ main.py
@@ -20,9 +20,11 @@ def main():
             print("Dimension metric should be an integer.")
             sys.exit()
 
-
+    print("Here is your starting grid:")
     grid = init_grid(dim)
     draw_grid(grid)
+
+    starting_lives = prompt_for_starting_lives(grid)
 
 
 def init_grid(dim):
@@ -66,6 +68,55 @@ def draw_grid(grid):
         draw_row(grid[row]) 
         draw_line()
     print('') 
+
+
+def prompt_for_starting_lives(grid):
+    """Prompts the user for which cells they wish to start live.
+
+    :param grid: The grid the cells are on.
+
+    :returns: An array with the coords of the starting live cells.
+    """
+    print("Where would you like to place your starting lives?")
+
+    grid_len = len(grid)  
+    print('grid len = ', grid_len)
+
+    lives = []
+
+    while True:
+        inp = input("Enter a cell in the form [y, x] or type 'END': ")
+
+        # Here is our break condition to end the input prompt loop
+        if inp == "END":
+            break
+
+        # checks the coordinates are valid
+        coord = inp.split(',')
+        if len(coord) != 2:
+            print("You must provide 2 coordinates.")
+            continue
+        else:
+            try:
+                coord[0] = int(coord[0])
+                coord[1] = int(coord[1])
+            except Exception as e:
+                print("Your coordinates must be integers.")
+                continue
+            
+            if coord[0] < 0 or coord[1] < 0:
+                print("Your coordinates cannot be negative.")
+                continue
+            elif coord[0] > grid_len - 1 or coord[1] > grid_len - 1:
+                print("Your coordinates must be within your grid.")
+            else:
+                lives.append(coord)
+                print(f"Coordinate [{coord[0]}, {coord[1]}] will start live.")
+    
+    print(f"Here are you starting live cells = {lives}.\n")
+
+    return lives
+
 
 
 
